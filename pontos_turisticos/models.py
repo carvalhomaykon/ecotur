1b1e3df916df5ac089745e5a33a2b707905096cd
Maykon Carvalho <carvalhomaykon566@gmail.com>
2025-08-27 16:53:37 -0300

    Ajustando interface e removendo arquivos não utilizaveis

--- pontos_turisticos/models.py
+++ pontos_turisticos/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.TextChoices):
+    CULTURAL = "cultural", "Cultural"
+    NATURAL = "natural", "Natural"
+    SEM_CATEGORIA = "sem_categoria", "Sem Categoria"
+
 class PontosTuristicos(models.Model):
     nome = models.CharField(max_length=100)
     latitude = models.FloatField()
     longitude = models.FloatField()
     descricao = models.TextField()
+    categoria = models.CharField(max_length=20, choices=Categoria.choices, default=Categoria.SEM_CATEGORIA)
 
     cep = models.CharField(max_length=8)
     endereco = models.CharField(max_length=100)
@@ -13,3 +18,8 @@ class PontosTuristicos(models.Model):
     complemento = models.CharField(max_length=100)
     cidade = models.CharField(max_length=100)
     estado = models.CharField(max_length=100)
+
+class ImagemPorPontoTuristico(models.Model):
+    ponto_turistico = models.ForeignKey(PontosTuristicos, on_delete=models.CASCADE, related_name="imagens")
+    imagem = models.ImageField(upload_to="pontos_turisticos/")
+
--- project/urls.py
+++ project/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('home.urls')),
     path('guia-turistico/', include('guia_turistico.urls')),
-    path('recompensas/', include('recompensas.urls')),
     path('rotas/', include('rotas.urls')),
     path('pontos-turisticos/', include('pontos_turisticos.urls')),
     path('avaliacao/', include('avaliacao.urls')),
